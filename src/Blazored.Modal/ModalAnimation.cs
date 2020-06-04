5dc659779d13ad263bd17489dc04b4449b86f42c
Lars Koopmans <larsk2009@live.nl>
2020-06-04 15:56:05 +0200

    Very preliminary verison of animation in/out for #166.

---
+++ src/Blazored.Modal/ModalAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazored.Modal
+{
+    public enum ModalAnimation
+    {
+        None = 0, // Set to 0 so it is definitely the default value
+        FadeIn,
+        FadeOut,
+        FadeInOut
+    }
+}
