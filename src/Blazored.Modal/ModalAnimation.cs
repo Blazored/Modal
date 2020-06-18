4d7dff1ca63873f59e3c4e78d59576bfa81c6bc5
Lars Koopmans <larsk2009@live.nl>
2020-06-18 16:38:20 +0200

    Add option for duration to animation

--- src/Blazored.Modal/ModalAnimation.cs
+++ src/Blazored.Modal/ModalAnimation.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Blazored.Modal
 {
-    public enum ModalAnimation
+    public class ModalAnimation
+    {
+        public ModalAnimationType Type { get; set; }
+
+        public double? Duration { get; set; }
+    }
+
+    public enum ModalAnimationType
     {
         None = 0, // Set to 0 so it is definitely the default value
         FadeIn,
