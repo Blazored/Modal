e9d104fe78d84538679c5393ac6b3faf85e95a85
Chris Sainty <chris.sainty@flagship-group.co.uk>
2020-06-18 07:57:10 +0100

    Modal service now returns IModalReference instead of ModalReference

---
+++ src/Blazored.Modal/IModalReference.cs
@@ -0,0 +1,13 @@
+﻿using Blazored.Modal.Services;
+using System.Threading.Tasks;
+
+namespace Blazored.Modal
+{
+    public interface IModalReference
+    {
+        Task<ModalResult> Result { get; }
+
+        void Close();
+        void Close(ModalResult result);
+    }
+}
