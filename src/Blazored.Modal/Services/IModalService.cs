26d5d86ad147b3ab8949be36038d1feaa2a1b42c
Lars Koopmans <larsk2009@live.nl>
2020-12-23 10:48:58 +0100

    Change ComponentBase to IComponent (#272)
    * Use `IComponent` instead of `ComponentBase`
    * Change the order of parameter docs to be the same order as parameters themselves

--- src/Blazored.Modal/Services/IModalService.cs
+++ src/Blazored.Modal/Services/IModalService.cs
@@ -8,27 +8,27 @@ namespace Blazored.Modal.Services
         /// <summary>
         /// Shows a modal containing the specified <typeparamref name="TComponent"/>.
         /// </summary>
-        IModalReference Show<TComponent>() where TComponent : ComponentBase;
+        IModalReference Show<TComponent>() where TComponent : IComponent;
 
         /// <summary>
         /// Shows a modal containing a <typeparamref name="TComponent"/> with the specified <paramref name="title"/> .
         /// </summary>
         /// <param name="title">Modal title</param>
-        IModalReference Show<TComponent>(string title) where TComponent : ComponentBase;
+        IModalReference Show<TComponent>(string title) where TComponent : IComponent;
 
         /// <summary>
         /// Shows a modal containing a <typeparamref name="TComponent"/> with the specified <paramref name="title"/> and <paramref name="options"/>.
         /// </summary>
         /// <param name="title">Modal title</param>
         /// <param name="options">Options to configure the modal</param>
-        IModalReference Show<TComponent>(string title, ModalOptions options) where TComponent : ComponentBase;
+        IModalReference Show<TComponent>(string title, ModalOptions options) where TComponent : IComponent;
 
         /// <summary>
         /// Shows a modal containing a <typeparamref name="TComponent"/> with the specified <paramref name="title"/> and <paramref name="parameters"/>.
         /// </summary>
         /// <param name="title">Modal title</param>
         /// <param name="parameters">Key/Value collection of parameters to pass to component being displayed</param>
-        IModalReference Show<TComponent>(string title, ModalParameters parameters) where TComponent : ComponentBase;
+        IModalReference Show<TComponent>(string title, ModalParameters parameters) where TComponent : IComponent;
 
         /// <summary>
         /// Shows a modal containing a <typeparamref name="TComponent"/> with the specified <paramref name="title"/>,
@@ -37,7 +37,7 @@ namespace Blazored.Modal.Services
         /// <param name="title">Modal title.</param>
         /// <param name="parameters">Key/Value collection of parameters to pass to component being displayed.</param>
         /// <param name="options">Options to configure the modal.</param>
-        IModalReference Show<TComponent>(string title, ModalParameters parameters = null, ModalOptions options = null) where TComponent : ComponentBase;
+        IModalReference Show<TComponent>(string title, ModalParameters parameters = null, ModalOptions options = null) where TComponent : IComponent;
 
         /// <summary>
         /// Shows a modal containing a <paramref name="component"/>.
@@ -55,16 +55,16 @@ namespace Blazored.Modal.Services
         /// <summary>
         /// Shows a modal containing a <paramref name="component"/> with the specified <paramref name="title"/> and <paramref name="options"/>.
         /// </summary>
-        /// <param name="title">Modal title.</param>
         /// <param name="component">Type of component to display.</param>
+        /// <param name="title">Modal title.</param>
         /// <param name="options">Options to configure the modal.</param>
         IModalReference Show(Type component, string title, ModalOptions options);
 
         /// <summary>
         /// Shows a modal containing a <paramref name="component"/> with the specified <paramref name="title"/> and <paramref name="parameters"/>.
         /// </summary>
-        /// <param name="title">Modal title.</param>
         /// <param name="component">Type of component to display.</param>
+        /// <param name="title">Modal title.</param>
         /// <param name="parameters">Key/Value collection of parameters to pass to component being displayed.</param>
         IModalReference Show(Type component, string title, ModalParameters parameters);
 
@@ -72,6 +72,7 @@ namespace Blazored.Modal.Services
         /// Shows a modal containing a <paramref name="component"/> with the specified <paramref name="title"/>, <paramref name="parameters"/>
         /// and <paramref name="options"/>.
         /// </summary>
+        /// <param name="component">Type of component to display.</param>
         /// <param name="title">Modal title.</param>
         /// <param name="parameters">Key/Value collection of parameters to pass to component being displayed.</param>
         /// <param name="options">Options to configure the modal.</param>
