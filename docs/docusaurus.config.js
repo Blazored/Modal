af266146a900acbdc41c2a93c0ef144fd394d1c9
Chris Sainty <chris@chrissainty.com>
2022-09-17 14:06:39 +0100

    Updated CI to build and deploy new docs site

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Blazored', // Usually your GitHub org/user name.
   projectName: 'Modal', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
